66137c09f87136bb6738610ff7df32768d367da6
venkat-rajgopal <venkatramani.r@gmail.com>
2021-12-04 13:05:07 +0100

    day3 part2

--- 2021/day03.py
+++ 2021/day03.py
@@ -25,8 +25,6 @@ def get_most_common(n_bit):
 
         bytes = list(map(int, get_byte(lines[i])))
 
-        # print(bytes)
-
         if bytes[n_bit] == 0:
             count_0 += 1
         elif bytes[n_bit] == 1:
@@ -66,3 +64,59 @@ epsilon_rate  = int(''.join(map(str, epsilon_rate)), 2)
 
 
 print(f"Power Consumtion : {gamma_rate * epsilon_rate}")
+
+
+# Part 2
+
+with open("./2021/inputs/input_day03.txt") as file:
+    # strip instead of split
+    data = [line.strip() for line in file]
+
+# create a copy for each 
+oxygen = data
+ox_gen_rating = 0
+
+# for the 12 digits
+for i in range(len(data[0])):
+    bit_counter = {'0': 0, '1': 0}
+
+    for number in oxygen:
+        # update the dict count if 1 or 0
+        bit_counter[number[i]] += 1
+
+    # get the most common
+    if bit_counter['1'] >= bit_counter['0']:
+        most_common = '1'
+    else:
+        most_common = '0'
+
+    # get the numbers which match the most common bit
+    oxygen = [n for n in oxygen if n[i] == most_common]
+
+    if len(oxygen) == 1:
+        ox_gen_rating = int(oxygen[0], 2)
+
+
+# similary for co2 scrubber rating do the east common
+co2 = data
+co2_rating = 0
+
+for i in range(len(data[0])):
+    bit_counter = {'0': 0, '1': 0}
+
+    for number in co2:
+        bit_counter[number[i]] += 1
+
+    if bit_counter['1'] < bit_counter['0']:
+        least_common = '1'
+
+    else:
+        least_common = '0'
+
+    co2 = [n for n in co2 if n[i] == least_common]
+
+    if len(co2) == 1:
+        co2_rating = int(co2[0], 2)
+
+
+print('Life support rating:', co2_rating * ox_gen_rating)
